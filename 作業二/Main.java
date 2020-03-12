dfeba5afa76d11817cff19b71f0b4df97948075f
s6538142 <43198277+s6538142@users.noreply.github.com>
2020-03-12 17:22:19 +0800

    Add files via upload

--- 作業二/Main.java
+++ 作業二/Main.java
@@ -4,6 +4,10 @@ public class Main {
         Sutdent sutdent1;
         sutdent1=new Sutdent();
         sutdent1.setName("44444");
+        sutdent1.setColor("black");
+        sutdent1.setId("1020235");
+        sutdent1.setGrade("三年級");
+        sutdent1.setNumber("10");
         System.out.println(sutdent1.showInfo());
     }
 }
--- 作業二/Sutdent.java
+++ 作業二/Sutdent.java
@@ -1,6 +1,6 @@
 public class Sutdent {
 
-        public String name,color,id;
+        public String name,color,id,grade,number;
 
         public Sutdent(){
             System.out.println("student1 object has created!");
@@ -8,12 +8,38 @@ public class Sutdent {
         public void setName(String newName){
             this.name=newName;
         }
-        public String getName(){
+        public String getName() {
             return this.name;
         }
-        public String showInfo(){
 
-            return this.getName()+"\t id"+"\t green";
+        public void setColor(String newColor){
+        this.color=newColor;
+
+        }
+        public String getColor(){
+        return this.color;
+    }
+    public void setId(String newId){
+        this.id=newId;
+
+    }
+    public String getId(){
+        return this.id;
+    }
+    public void setGrade(String newGrade){
+        this.grade=newGrade;
+    }
+    public String getGrade() {
+        return this.grade;
+    }
+    public void setNumber(String newNumber){
+        this.number=newNumber;
+    }
+    public String getNumber() {
+        return this.number;
+    }
+        public String showInfo(){
+            return this.getName()+"\t"+this.getColor()+"\t"+this.getId()+"\t"+this.getGrade()+"\t"+this.getNumber();
         }
     }
 
