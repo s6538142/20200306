a7df2df50e0f1651cd704afe527a08d65ed14e27
s6538142 <43198277+s6538142@users.noreply.github.com>
2020-03-12 17:32:01 +0800

    Add files via upload

--- 作業1/Book1.java
+++ 作業1/Book1.java
@@ -1,5 +1,5 @@
 public class Book1 {
-    public String name,color,id;
+    public String name,color,id,category,price;
 
     public Book1(){
         System.out.println("Book1 object has created!");
@@ -10,8 +10,36 @@ public class Book1 {
     public String getName(){
         return this.name;
     }
+    public void setColor(String newColor){
+        this.color=newColor;
+
+    }
+    public String getColor(){
+        return this.color;
+    }
+    public void setId(String newId){
+        this.id=newId;
+
+    }
+    public String getId(){
+        return this.id;
+    }
+    public void setCategory(String newCategory){
+        this.category=newCategory;
+
+    }
+    public String getCategory(){
+        return this.category;
+    }
+    public void setPrice(String newPrice){
+        this.price=newPrice;
+
+    }
+    public String getPrice(){
+        return this.price;
+    }
     public String showInfo(){
 
-        return this.getName()+"\t id"+"\t green";
+        return this.getName()+"\t"+this.getId()+"\t"+this.getColor()+"\t"+this.getCategory()+"\t"+this.getPrice();
     }
 }
--- 作業1/Main.java
+++ 作業1/Main.java
@@ -1,9 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
+//        Sutdent sutdent1;
+//        sutdent1=new Sutdent();
+//        sutdent1.setName("44444");
+//        sutdent1.setColor("black");
+//        sutdent1.setId("1020235");
+//        sutdent1.setGrade("三年級");
+//        sutdent1.setNumber("10");
+//        System.out.println(sutdent1.showInfo());
         Book1 book1;
         book1=new Book1();
         book1.setName("44444");
+        book1.setColor("black");
+        book1.setId("編號:105");
+        book1.setCategory("科目:Math");
+        book1.setPrice("售價300元");
         System.out.println(book1.showInfo());
     }
 }
